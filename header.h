d97584a16df2370b1d8abe535647ef4fc6d3a82a
AndrewGenualdo <143838993+AndrewGenualdo@users.noreply.github.com>
2024-02-02 13:29:27 -0500

    initial commit

---
+++ header.h
@@ -0,0 +1,23 @@
+#ifndef _HEADER_H
+#define _HEADER_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+using namespace std;
+const int MAX = 20, SECTIONS = 2, GRADE = 12;
+const string FILENAME = "students.txt";
+const string NEXT_SECTION = "*****";
+void assignGrade(Student data[][SECTIONS], const int size[SECTIONS]);
+void computeAverage(Student data[][SECTIONS], const int size[SECTIONS]);
+void computeAverage(const Student data[][SECTIONS], const int size[SECTIONS],
+double averages[SECTIONS]);
+void counteGrade(const Student data[][SECTIONS], const int size[SECTIONS],
+int grade[GRADE][SECTIONS]);
+void displayData(const Student data[][SECTIONS], const int size[SECTIONS]);
+void displayStat(const Student data[][SECTIONS], const int size[SECTIONS],
+const double averages[SECTIONS], const int grade[GRADE]
+[SECTIONS]);
+void loadData(Student data[][SECTIONS], int size[SECTIONS]);
+void sortData(Student data[][SECTIONS], const int size[SECTIONS]);
+#endif
---
+++ lab2.cpp
@@ -0,0 +1,23 @@
+#include "header.h"
+
+int main()
+{
+   Student data[MAX][SECTIONS];
+   int     size[SECTIONS];
+   double  averages[SECTIONS];
+   int     letterGrade[GRADE][SECTIONS];
+
+   cout.setf(ios::fixed | ios::showpoint);
+   cout.precision(1);
+
+   loadData(data, size);
+   computeAverage(data, size);
+   assignGrade(data, size);
+   sortData(data, size);
+   displayData(data, size);
+   computeAverage(data, size, averages);
+   counteGrade(data, size, letterGrade);
+   displayStat(data, size, averages, letterGrade);
+
+   return 0;
+}
